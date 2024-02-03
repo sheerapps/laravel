ef04b08a3fc794e46f19dd9704235b1f6484fbdf
sheerapps <sheerapps@gmail.com>
2024-02-02 19:41:45 -0800

    update tsheer

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,5 @@ Route::get('/data-by-history', [ApiController::class, 'getDataBySearch']);
 Route::get('/data-by-dictionary', [ApiController::class, 'getDicByData']);
 Route::get('/save-by-date/{date}', [ApiController::class, 'saveData']);
 Route::get('/data-draw-date', [ApiController::class, 'getDrawdateData']);
+//tsheer
+Route::get('/tmain-by-date/{date}', [ApiController::class, 'getTMainByDate']);
