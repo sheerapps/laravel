1c0f7d7d35713f2d82a633827c0e3f90a070160a
sheerapps <sheerapps@gmail.com>
2024-04-06 08:58:13 +0000

    g

---
+++ app/Sheerlive.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sheerlive extends Model
+{
+    protected $table = "sheerlive";
+    protected $fillable = ["date","data"];
+}
