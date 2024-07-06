7f40302fd599aa5abf882c876ca3c1defd199204
sheerapps <sheerapps@gmail.com>
2024-07-06 07:26:54 -0700

    g

--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,9 @@ Route::get('/tsave/{date}', [ApiController::class, 'saveLiveDB']);
 
 Route::get('/data-by-date/{date}', [ApiController::class, 'getDataByDate']);
 Route::get('/main-by-date/{date}', [ApiController::class, 'getMainByDate']);
-Route::get('/main-by-date-v110/{date}', [ApiController::class, 'getMainByDateV1_2_0']);
-Route::get('/main-by-date-v120/{date}', [ApiController::class, 'getMainByDateV1_2_0']); // no use
-
+Route::get('/main-by-date-v110/{date}', [ApiController::class, 'getMainByDateV1_2_0']); //update 1_1_0 to 1_2_0
+// Route::get('/main-by-date-v120/{date}', [ApiController::class, 'getMainByDateV1_2_0']); // no use
+Route::get('/tmain-by-date/{date}', [ApiController::class, 'getTMainByDateV1_2_0']); //update getTMainByDate to 1_2_0 code
 
 Route::get('/data-by-history', [ApiController::class, 'getDataBySearch']);
 Route::get('/data-by-history-v120', [ApiController::class, 'getDataByAdvanceSearch']);
@@ -35,7 +35,7 @@ Route::get('/save-by-live', [ApiController::class, 'saveLive']);
 Route::get('/data-draw-date', [ApiController::class, 'getDrawdateData']);
 Route::get('/all-book-data', [ApiController::class, 'getBookAll']);
 //tsheer
-Route::get('/tmain-by-date/{date}', [ApiController::class, 'getTMainByDate']);
+
 Route::get('/t', function() {
     $ch = curl_init(); 
     curl_setopt( $ch, CURLOPT_URL, "https://backend.4dnum.com/api/v1/result/2024-06-06");
