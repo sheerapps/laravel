2929986ca29fbccd1532ca809d5168f705ae82f8
sheerapps <sheerapps@gmail.com>
2024-12-23 17:29:33 -0800

    g

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -38,7 +38,7 @@ class ApiController extends Controller
     public function saveDataV1_2_0($date){
         // $data = $this->getMainByDateV1_2_0($date);
         $data = $this->getMainByDateV1_3_0($date);
-        $type = "live"; //past
+        $type = "past"; //past or live
         foreach ($data as $item) {
             //save live results
             if($type == "live" && isset($item['fdData'])){
