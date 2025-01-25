9decc952cbb45ae722a853c391ef0776a2faf63a
sheerapps <sheerapps@gmail.com>
2025-01-25 12:33:16 +0800

    g

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Fine Enterprise
+                    Fine Enterprise1
                 </div>
 
                 <div class="links">
