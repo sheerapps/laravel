a245e98426c70e558ba7cb4c86ebba245414de61
sheerapps <sheerapps@gmail.com>
2025-03-14 15:59:54 +0800

    g

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4585,7 +4585,7 @@ class ApiController extends Controller
                 ],
             ];
         }else{ //fetch from db
-            $records = Sheerlive::whereIn("id", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])->get(['data']);
+            $records = Sheerlive::whereIn("id", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22,23,24])->get(['data']);
             $r = [];
             foreach ($records as $key => $record) {
                 $r[] = json_decode($record->data,true);
