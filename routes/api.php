d7bd78500e28f1a56f431bfb5c1928f86afc4ac0
sheerapps <sheerapps@gmail.com>
2025-05-01 15:40:46 +0800

    g

--- routes/api.php
+++ routes/api.php
@@ -23,16 +23,17 @@ Route::get('/tsave/{date}', [ApiController::class, 'saveDataV1_2_0']);
 Route::get('/data-by-date/{date}', [ApiController::class, 'getDataByDate']);
 Route::get('/main-by-date/{date}', [ApiController::class, 'getMainByDate']);
 
-Route::get('/main-by-date-v150/{date}', [ApiController::class, 'getMainByDateV1_5_0']); //results from db
-Route::get('/main-by-date-v140/{date}', [ApiController::class, 'getMainByDateV1_5_0']); //added W3/W7 results
+Route::get('/main-by-date-v150/{date}', [ApiController::class, 'getMainByDateV1_5_0']); //results from db test
+Route::get('/main-by-date-v140/{date}', [ApiController::class, 'getMainByDateV1_5_0']); //results from db in use
 // Route::get('/main-by-date-v140/{date}', [ApiController::class, 'getMainByDateV1_4_0']); //added W3/W7 results
 Route::get('/main-by-date-v130/{date}', [ApiController::class, 'getMainByDateV1_3_0']); //added GT/DL/MH results
 Route::get('/main-by-date-v110/{date}', [ApiController::class, 'getMainByDateV1_2_0']); //update 1_1_0 to 1_2_0
 Route::get('/main-by-date-v120/{date}', [ApiController::class, 'getMainByDateV1_2_1']); // new update app 7/7/2024 change results arrangement only
 Route::get('/tmain-by-date/{date}', [ApiController::class, 'getTMainByDateV1_2_0']); //update getTMainByDate to 1_2_0 code
 
-Route::get('/tmain-by-date-v3/{date}', [ApiController::class, 'getTMainByDateV1_4_0']); //results from db
-Route::get('/tmain-by-date-v2/{date}', [ApiController::class, 'getTMainByDateV1_3_0']); //W3/W4 results
+Route::get('/tmain-by-date-v3/{date}', [ApiController::class, 'getTMainByDateV1_4_0']); //results from db test
+Route::get('/tmain-by-date-v2/{date}', [ApiController::class, 'getTMainByDateV1_4_0']); //results from db in use
+// Route::get('/tmain-by-date-v2/{date}', [ApiController::class, 'getTMainByDateV1_3_0']); //W3/W4 results
 
 Route::get('/save-to-db/{date}', [ApiController::class, 'saveLiveDB']);
 Route::get('/save-to-db2/{date}', [ApiController::class, 'saveLiveDB2']);
